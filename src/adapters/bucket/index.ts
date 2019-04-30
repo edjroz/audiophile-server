b473918ed46788cb8e11a1d85c20718568d845e8
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2019-04-30 00:26:21 -0400

    add bucket repository

---
+++ src/adapters/bucket/index.ts
@@ -0,0 +1,15 @@
+import mongodb from 'mongodb'
+
+export default class BucketRepository {
+    bucket: mongodb.GridFSBucket;
+    constructor(db: mongodb.Db) {
+      this.bucket = new mongodb.GridFSBucket(db, {bucketName: 'songs'});
+    }
+    getSongStream(trackID: mongodb.ObjectID): mongodb.GridFSBucketReadStream {
+      return this.bucket.openDownloadStream(trackID)
+    }
+    writeSongStream(trackName: string): mongodb.GridFSBucketWriteStream {
+      return this.bucket.openUploadStream(trackName)
+    }
+
+}
